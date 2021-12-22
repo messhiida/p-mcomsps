8d936179b24cf46d816f940c8d018287ed744a84
messhiida <iida.0430@gmail.com>
2021-12-23 08:28:37 +0900

    CSD storage 復活

--- painless/painless-src/similarity/similarity.cpp
+++ painless/painless-src/similarity/similarity.cpp
@@ -20,6 +20,6 @@ CSD CsdBuffer::catchCSD()
 }
 void CsdBuffer::recordCSD(CSD input, int id)
 {
-    //csdStorage[id] = input;
-    //printf("recordCSD: [%d]%d\n", id, csdStorage.size());
+    csdStorage[id] = input;
+    printf("recordCSD: [%d]%d\n", id, csdStorage.size());
 }
